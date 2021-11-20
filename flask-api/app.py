81eff12f67de9975cac87e60a6d7e9897c5d9f82
Pranav <Pranav-Kumar-1>
2021-11-20 15:49:59 +0530

    backend-api-setup

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,12 +1,22 @@
 from flask import Flask, request
 from flask_cors import CORS
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+client = PyMongo(app, uri = "mongodb+srv://root:123@cluster0.zmfng.mongodb.net/account_me_out?authSource=admin&replicaSet=atlas-80odye-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
+db = client.db
+
+transactions = db['transactions']
+
+print(list(transactions.find({})))
+
 CORS(app)
 
-@app.route("/hi")
+@app.route("/hi", methods = ["POST"])
 def hi():
-    return "Hi"
+    transactions.insert_one({'example' : "Pranav"})
+    return {"message" : "success"}
 
 if __name__ == "__main__":
     app.run()
