99158b4379596c9df51a731fd4ce9cbaca94a8a9
Pranav <Pranav-Kumar-1>
2021-11-27 16:27:48 +0530

    testing

--- ui_test/selenium_testing.py
+++ ui_test/selenium_testing.py
@@ -15,18 +15,19 @@ host="http://localhost:3000/"
 def test_searchWithButton(sb):
     # print(host)
     sb.open("http://localhost:3000/")
-    sb.maximize_window()
+    sb.maximize_window()    
 
     # driver = webdriver.Chrome(executable_path="/home/pranav/Desktop/SE Project/ui_test/chromedriver")
     # driver.open("http://localhost:3000/")
     # driver.maximize_window()
 
-    try:
-        sb.find_element(By.LINK_TEXT("Apply For Loan"))
-
-        assert True
-    except:
-        assert False
+    # try:
+    #     if sb.find_element("Account Me Out", By.LINK_TEXT):
+    #         assert True
+    #     else:
+    #         assert False
+    # except:
+    #     assert False
 
     # try:
     #     nav_link = sb.find_element("fa fa-suitcase text-black", By.CLASS_NAME).text
