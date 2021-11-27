1c2ad888e095dbbda1fb6ec905ff315883913b71
Hrishikesh Lamdade <hrishilamdade@gmail.com>
2021-11-27 16:37:16 +0530

    Commit

--- Account-Me-Out/components/Mic/SpeechReco.js
+++ Account-Me-Out/components/Mic/SpeechReco.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import axios from 'axios';
+// import Siriwave from 'react-siriwave';
+
 const appId = '7485fb98-8557-4375-8387-ec394333c297';
 const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
 SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
@@ -13,7 +15,7 @@ const Mic = () => {
     resetTranscript,
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
-  const startListening = () => SpeechRecognition.startListening({ continuous: true });
+  const startListening = () => SpeechRecognition.startListening({ continuous :  true });
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
@@ -27,7 +29,7 @@ const Mic = () => {
 
         const url = "http://localhost:8000/vpaInput";
 
-        const temp = transcript;
+        const temp = transcript ;
 
         // resetTranscript();
         console.log('transcript is: ',transcript);
